10d9523e3f0bf9464384ff7bb5174eff424ef8a0
mshah1819 <mshah1819@gmail.com>
2015-08-09 20:39:16 -0700

    Initial commit with setting up environment for devlopment

---
+++ setProdDevRev.bat
@@ -0,0 +1,3 @@
+set PRODREVDEV_WRK=C:\ProdRevDev
+setenv.bat
+set PATH=%PATH%;%PRODREVDEV_WRK%/main;
---
+++ setenv.bat
@@ -0,0 +1,6 @@
+set JAVA_HOME=%PRODREVDEV_WRK%\tools\windows\Java\jdk-1_8_51
+set JRE_HOME=%PRODREVDEV_WRK%\tools\windows\Java\jdk-1_8_51
+set ANT_HOME=%PRODREVDEV_WRK%\tools\common\ant\apache-ant-1.9.6
+
+set PATH=%PATH%;%JAVA_HOME%/bin;%ANT_HOME%/bin;
+
